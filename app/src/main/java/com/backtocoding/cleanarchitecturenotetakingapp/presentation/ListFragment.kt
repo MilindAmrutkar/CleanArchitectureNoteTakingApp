9ef2e5432cafbc43072ccf66bae33e1da312296d
MilindAmrutkar <milind27.amrutkar@gmail.com>
2023-07-08 20:31:28 +0530

    Done with Navigation

--- app/src/main/java/com/backtocoding/cleanarchitecturenotetakingapp/presentation/ListFragment.kt
+++ app/src/main/java/com/backtocoding/cleanarchitecturenotetakingapp/presentation/ListFragment.kt
@@ -5,14 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.backtocoding.cleanarchitecturenotetakingapp.R
+import androidx.navigation.Navigation
+import com.backtocoding.cleanarchitecturenotetakingapp.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
+//        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.addNote.setOnClickListener { goToNoteDetails() }
+    }
+
+    /**
+     * This function takes us to that detail screen
+     */
+    private fun goToNoteDetails(id: Long = 0L) {
+        val action = ListFragmentDirections.actionGoToNote(id)
+        Navigation.findNavController(binding.notesListView).navigate(action)
     }
 }
--- app/src/main/java/com/backtocoding/cleanarchitecturenotetakingapp/presentation/NoteFragment.kt
+++ app/src/main/java/com/backtocoding/cleanarchitecturenotetakingapp/presentation/NoteFragment.kt
@@ -5,14 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.backtocoding.cleanarchitecturenotetakingapp.R
+import androidx.navigation.Navigation
+import com.backtocoding.cleanarchitecturenotetakingapp.databinding.FragmentNoteBinding
 
 class NoteFragment : Fragment() {
+    private var _binding: FragmentNoteBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_note, container, false)
+        _binding = FragmentNoteBinding.inflate(inflater, container, false)
+        return binding.root
+//        return inflater.inflate(R.layout.fragment_note, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.checkButton.setOnClickListener { Navigation.findNavController(it).popBackStack() }
     }
 }
